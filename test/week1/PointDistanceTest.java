d47849ed42477706a49b0c139e6c0b236f99e049
JJELEMENT <sorrawich.ss@mail.kmutt.ac.th>
2018-02-07 08:50:01 +0700

    week3 & week4 warmup

---
+++ test/week1/PointDistanceTest.java
@@ -0,0 +1,43 @@
+package week1;
+
+import myproject.week1.model.Point;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class PointDistanceTest {
+    
+    private Point source;
+    private Point target;
+    
+    public PointDistanceTest() {
+        source = new Point();
+        target = new Point();
+    }
+    
+    @Test
+    public void distanceTest() {
+        double expected = 1.4142135623730951;
+        
+        source.setX(5);
+        source.setY(2);
+        target.setX(4);
+        target.setY(5);
+        
+        double actual = source.getDistanceCalculate(target);
+        assertEquals(expected, actual, 0.1);
+    }
+    
+    @Test
+    public void distanceTest2() {
+        double expected = 0;
+        
+        source.setX(0);
+        source.setY(0);
+        target.setX(0);
+        target.setY(0);
+        
+        double actual = source.getDistanceCalculate(target);
+        assertEquals(expected, actual, 0.1);
+    }
+}
---
+++ test/week2/MonthTest.java
@@ -0,0 +1,71 @@
+package week2;
+
+import myproject.week2.Month;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MonthTest {
+
+    private Month month;
+
+    public MonthTest() {
+        month = new Month();
+    }
+
+    @Test
+    public void find31DaysOfMonth() {
+        int expected = 31;
+        int actual = month.findDaysOfMonth(1);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void find30DaysOfMonth() {
+        int expected = 30;
+        int actual = month.findDaysOfMonth(4);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void find28DaysOfMonth() {
+        int expected = 28;
+        int actual = month.findDaysOfMonth(2);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void findDayInZeroMonth() {
+        int expected = -1;
+        int actual = month.findDaysOfMonth(0);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void findDayInThirteenMonth() {
+        int expected = -1;
+        int actual = month.findDaysOfMonth(13);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void findNameOfInvalidMonth() {
+        String expected = null;
+        String actual = month.findNameOfMonth(13);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNameOfFirstMonth() {
+        String expected = "Jan";
+        String actual = month.findNameOfMonth(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNameOfSixthMonth() {
+        String expected = "Jun";
+        String actual = month.findNameOfMonth(6);
+        assertEquals(expected, actual);
+    }
+}
