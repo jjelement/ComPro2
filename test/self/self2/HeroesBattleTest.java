d6a66a120383289b0c689716a114e4f24d361c36
JJELEMENT <jjelementza@gmail.com>
2018-02-26 23:00:24 +0700

    Self2-2

--- test/self/self2/HeroesBattleTest.java
+++ test/self/self2/HeroesBattleTest.java
@@ -3,12 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package heroesbattle;
+package self.self2;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import sorrawich.self.self2.Armor;
 import sorrawich.self.self2.Hero;
